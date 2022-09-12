81dc3aaf9bc569bda1440f578f7ee8331a531430
serhii.lomov <serhii.lomov@itomy.ch>
2022-09-13 01:07:19 +0300

    Initial state

--- CurvesDrawer/ContentView.swift
+++ CurvesDrawer/ContentView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private let strokeStyle = StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round)
+    private let backColor = Color(red: 0.75, green: 0.67, blue: 0.88)
+    private let strokeColor = Color(red: 0.6, green: 0.25, blue: 0.6)
+    private let duration = 4.0
+
+    @State private var progress: DrawingProgress = .zero
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        let opacity: CGFloat = progress == .zero ? 0 : 1
+        CenteredGeometryReader {
+            backColor
+                .ignoresSafeArea(.all)
+            DrawableCurvesView(elements: curves)
+                .opacity(opacity)
+                .animation(nil, value: opacity)
+                .drawingProgress(progress)
+                .environment(\.drawingWidth, 4)
+                .animation(.linear(duration: 4), value: progress)
+                .foregroundColor(strokeColor)
+                .aspectRatio(1, contentMode: .fit)
+                .padding()
         }
+        .onTapGesture {
+            progress = .full
+        }
+    }
+
+    private var curves: [DrawableCurve] {
+        let curve = BezierCurve(x0: -0.5, y0: -0.5, x1: -0.5, y1: 0.5, x2: 0.5, y2: -0.5, x3: 0.5, y3: 0.5)
+        let drawable = DrawableCurve(curve: curve, startAt: 0, finishAt: 1)
+        return [drawable]
     }
 }
 
---
+++ Views/DrawableCurvesView.swift
@@ -0,0 +1,37 @@
+//
+//  DrawableCurvesView.swift
+//  TIA
+//
+//  Created by serhii.lomov on 11.06.2022.
+//
+
+import SwiftUI
+
+struct DrawableCurvesView: View {
+
+    let elements: [DrawableCurve]
+    var lineCap: CGLineCap = .round
+    @Environment(\.drawingProgress) var drawingProgress
+    @Environment(\.drawingWidth) var drawingWidth
+
+    var body: some View {
+        CenteredGeometryReader { geometry in
+            ForEach(elements, id: \.id) { element in
+                Path(curve: element.curve, geometry: geometry)
+                    .trimmedPath(from: 0, to: relativeProgress(element))
+                    .stroke(style: strokeStyle(element))
+            }
+        }
+    }
+
+    private func relativeProgress(_ element: DrawableCurve) -> CGFloat {
+        let length = element.finishAt - element.startAt
+        let relative = (drawingProgress.value - element.startAt) / length
+        return relative.normalized(min: 0, max: 1)
+    }
+
+    private func strokeStyle(_ element: DrawableCurve) -> StrokeStyle {
+        let width = element.widthMult * drawingWidth
+        return StrokeStyle(lineWidth: width, lineCap: lineCap)
+    }
+}
